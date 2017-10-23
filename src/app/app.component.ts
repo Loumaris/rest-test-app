9a4e01ffab815f7ab818f45d1bff1d5363ab75b1
Christian Heimke <christian.heimke@loumaris.com>
2017-10-23 10:57:46 +0200

    added api request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'rest test app';
+  obj: any;
+  obj_str: any;
+
+  constructor(private http: Http) {
+
+         this.getJSON().subscribe(
+           data => {
+             this.obj = data
+             this.obj_str = JSON.stringify(data)
+           },
+           error => {
+             console.log(error)
+           });
+    }
+
+    public getJSON(): Observable<any> {
+         return this.http.get("http://localhost:4567/echo")
+                         .map((res:any) => res.json());
+
+     }
 }
